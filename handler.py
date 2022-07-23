fbb80fb6b9c57e34d90f2c22cf7c20454366b03c
Ritesh <ritesh_tarway@icloud.com>
2022-07-23 13:09:14 -0500

    changed function name

--- handler.py
+++ handler.py
@@ -1,7 +1,7 @@
 import json
 
 
-def hello(event, context):
+def helloNew(event, context):
     body = {
         "message": "Go Serverless v1.0.0! Your function executed successfully!",
         "input": event
